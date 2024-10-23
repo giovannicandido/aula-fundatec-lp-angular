904e5dca94b37bc4229471301013597787cbe7c2
Giovanni Silva <git@gsilva.pro>
2024-10-23 20:21:14 -0300

    lista de livros mockup

--- src/app/modules/livro/livro.module.ts
+++ src/app/modules/livro/livro.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { LivroListComponent } from "./livro-list/livro-list.component";
 import { LivroCommentsComponent } from "./livro-comments/livro-comments.component";
 import { LivrariaPageComponent } from "./pages/livraria-page.component";
+import { LivroComponent } from "./livro/livro.component";
 
 @NgModule(
     {
-       declarations: [LivroListComponent, LivroCommentsComponent, LivrariaPageComponent],
+       declarations: [LivroListComponent, LivroCommentsComponent, LivrariaPageComponent, LivroComponent],
        exports: [LivrariaPageComponent]
     }
 )
